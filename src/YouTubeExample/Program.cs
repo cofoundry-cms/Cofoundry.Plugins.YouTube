8b304b4f952244216e215f19b685803f18f1a61c
Joel Mitchell <me@joelmitchell.co.uk>
2018-08-31 09:59:12 +0100

    Updated to Cofoundry 0.4 and .NET Core 2.1

--- src/YouTubeExample/Program.cs
+++ src/YouTubeExample/Program.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace YouTubeExample
 {
@@ -14,16 +13,15 @@ namespace YouTubeExample
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .ConfigureAppConfiguration((hostContext, config) =>
-                {
-                    config.AddJsonFile("appsettings.local.json", optional: true);
-                })
-                .Build();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost
+            .CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .ConfigureAppConfiguration((hostContext, config) =>
+            {
+                config.AddJsonFile("appsettings.local.json", optional: true);
+            });
     }
 }
--- src/YouTubeExample/Startup.cs
+++ src/YouTubeExample/Startup.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Cofoundry.Web;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
